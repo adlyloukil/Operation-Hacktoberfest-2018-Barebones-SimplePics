a494ec7c41debd6011bd5ad2a10d3aac9a9732ea
Sara Tan <saratan@mac.com>
2018-10-20 16:24:59 +0100

    Scroll user to top of page when banner is clicked

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict";
 
-const modal = document.getElementById("myModal");
+// Scroll user to top of page when banner is clicked
+const banner = document.getElementsByClassName("header-img")[0];
 
-//const modalDiv = document.getElementById
+banner.onclick = function() {
+  window.scrollTo(0, 0);
+}
+
+const modal = document.getElementById("myModal");
 const img = document.getElementsByClassName("img-fluid");
 const modalImg = document.getElementById("img01");
 
